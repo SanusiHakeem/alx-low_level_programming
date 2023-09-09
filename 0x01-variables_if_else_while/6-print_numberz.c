867c207ce363119f605e4865f964af00fe8943bf
HakeemSanusi <sanusihakeem78@gmail.com>
2023-09-09 09:11:06 -0700

    code review

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/** 
+ * main - main function
+ * Description:printing number from 0 to 10
+ * Return: 0
+ */
+
+int main(void)
+{
+	int count = 0;
+	while (count < 10)
+	{
+		putchar((count % 10) + '0');
+		count++;
+	}
+	putchar('\n');
+	return (0);
+}
