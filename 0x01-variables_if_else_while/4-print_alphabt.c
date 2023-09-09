0be3889e08eb26023d9ff9f11b3619cb764cccad
HakeemSanusi <sanusihakeem78@gmail.com>
2023-09-09 08:21:44 -0700

    code review

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,16 +6,16 @@
  */
 int main(void)
 {
-        char alphabet = 'a';
-
-        while (alphabet <= 'z')
+	char alphabet = 'a';
+	
+	while (alphabet <= 'z')
 	{
 		if (alphabet != 'q' && alphabet != 'e')
 		{
 			putchar(alphabet);
 		}
-                alphabet++;
-        }
-        putchar('\n');
-        return (0);
+		alphabet++;
+	}		
+	putchar('\n');
+	return (0);
 }
