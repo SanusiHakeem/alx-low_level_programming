a2645b24fa5d1379fa747dc0795bcccf9c2e390c
HakeemSanusi <sanusihakeem78@gmail.com>
2023-09-09 09:24:41 -0700

    code review

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main- main function
+ * Description:printing all alphabet in reverse
+ * Return: 0
+ */
+
+int main(void)
+{
+	char letter = 'z';
+	while (letter >= 'a')
+	{
+		putchar(letter);
+		letter--;
+	}
+	putchar('\n');
+	return (0);
+}
