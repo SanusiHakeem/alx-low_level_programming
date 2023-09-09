584e669f393da06844a02760238c4539c3a6f2df
HakeemSanusi <sanusihakeem78@gmail.com>
2023-09-09 08:46:50 -0700

    code review

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Description:printing number from 0 to 10
+ * Return: 0
+ */
+
+int main(void)
+{
+	int count = 0;
+	while (count < 10)
+	{
+		printf("%d",count);
+		count++;
+	}
+	printf("\n");
+	return (0);
+}
