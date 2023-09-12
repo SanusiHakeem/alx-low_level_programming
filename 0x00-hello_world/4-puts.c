aded59ad5247c448d0d65dbe8b1578f1561c9390
HakeemSanusi <sanusihakeem78@gmail.com>
2023-09-11 21:16:14 -0700

    code review

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - main function "This program print programming is like building a multi
+*	lingual puzzle.
+* Return - Return 0
+*/
+
+int main(void)
+{
+	puts("\"Programming is like bulding a multilingual puzzle");
+	return (0);
+}
