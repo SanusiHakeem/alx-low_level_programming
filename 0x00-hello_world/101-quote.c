a5fae60da29ae9f2e4550df12011ef109eed9b03
HakeemSanusi <sanusihakeem78@gmail.com>
2023-09-11 21:55:02 -0700

    code review

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - main function print and that piece of art is useful" - Dora Korpar, 
+*	2015-10-19
+* Return - 1 i.e success
+*/
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+			, 58);
+	return (1);
+}
