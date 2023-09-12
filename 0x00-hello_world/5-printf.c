8a50a67b8459f6f2a1684b1c9ac264ec1549a5bd
HakeemSanusi <sanusihakeem78@gmail.com>
2023-09-11 21:29:29 -0700

    code review

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - main function "This program print with proper grammar, but the outcome
+*	is a piece of art
+* Return - Return 0
+*/
+
+int main(void)
+{
+	printf("wiht proper grammar, but the outcome is a piece of art");
+	printf("\n");
+	return (0);
+}
