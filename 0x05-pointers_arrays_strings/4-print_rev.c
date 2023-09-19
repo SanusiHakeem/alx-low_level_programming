92103d793f280e4bcf67f112adacccb91ca80ccf
HakeemSanusi <sanusihakeem78@gmail.com>
2023-09-18 21:06:14 -0700

    code review

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ *print_rev - print string in reverse
+ *@str: string pointer
+ *Return: counter
+ */
+int print_rev(char *str)
+{
+	char *z = str;
+	int i;
+	int first_count = 0;
+	int count = 0;
+
+	while (*str != '\0')
+	{
+		str++;
+		first_count++;
+	}
+
+	i = first_count - 1;
+	while (i >= 0)
+	{
+		_putchar(*(z + i));
+		i--;
+		count++;
+	}
+
+	return (count);
+}
